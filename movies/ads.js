0f6690abee7a4d544404a6df60fe2e195f0b7048
Eric <58240560+naisanzaa@users.noreply.github.com>
2025-08-12 04:56:15 -0400

    Update ads.js

--- movies/ads.js
+++ movies/ads.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         rophim.me
+// @name         movies
 // @namespace    http://tampermonkey.net/
 // @version      2025-08-11
 // @description  try to take over the world!
