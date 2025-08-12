b70cdf9262cce95566b71e8f6dd3e1fbae6ec83d
Eric <58240560+naisanzaa@users.noreply.github.com>
2025-08-12 04:44:31 -0400

    Create rophim.me.js

---
+++ rophim.me/rophim.me.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         rophim.me
+// @namespace    http://tampermonkey.net/
+// @version      2025-08-11
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.rophim.me/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=rophim.me
+// @grant        none
+// ==/UserScript==
+
+
+function hideStuff() {
+
+    try {
+        const className = 'is-image';
+        if (document.getElementsByClassName(className)[0]) {document.getElementsByClassName(className)[0].remove()}; console.log(`[main] :: removed :: ${ className }`)} catch (error) {}
+
+}
+
+setInterval(hideStuff, 250);
