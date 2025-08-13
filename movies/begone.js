c8a386fa75fb5fbe33051756af8eca711b1358c2
Eric <58240560+naisanzaa@users.noreply.github.com>
2025-08-13 07:03:00 +0700

    Create begone.js

---
+++ movies/begone.js
@@ -0,0 +1,130 @@
+// ==UserScript==
+// @name         movies3
+// @namespace    http://tampermonkey.net/
+// @version      2025-08-11
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.rophim.me/*
+// @match        https://www.olevod.com/*
+// @match        https://www.yfsp.tv/*
+// @match        https://www.zhuimj.tv/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=rophim.me
+// @grant        none
+// ==/UserScript==
+
+
+
+class SiteClass {
+    constructor(domain = 'generic', classes = [], ids = []) {
+        this.domain = domain;
+        this.classes = classes;
+        this.ids = ids;
+    }
+
+    hide_classes() {
+        for (const className of this.classes) {
+            const classSearch = document.getElementsByClassName(className);
+            if (classSearch.length) {
+                for (let i = 0; i < classSearch.length; i++) {
+                    if (classSearch[i]) {
+                        classSearch[i].remove();
+                        console.log(`[tampermonkey] :: [begone.js] :: ${ this.domain } :: removed :: class :: ${className}`);
+                    }}}}}
+
+    hide_ids() {
+        for (const idName of this.ids) {
+            const idSearch = document.getElementById(idName);
+            if (idSearch) {
+                idSearch.remove();
+                console.log(`[tampermonkey] :: [begone.js] :: ${ this.domain } :: removed :: id :: ${idName}`);
+            }}}
+
+    hide() {
+        this.hide_classes();
+        this.hide_ids();
+    }
+
+}
+
+
+
+(function() {
+    'use strict';
+
+
+    const olevod = new SiteClass(
+        'olevod.com',
+        [
+            'pc-footers',
+        ],
+        [
+
+        ]
+    )
+
+
+    const rophim = new SiteClass(
+        'rophim.me',
+        [
+            'footer-elements',
+            'fade v-modal d-modal sspp-modal modal show',
+            'my-area',
+            'sspp-area is-post',
+        ],
+        [
+
+        ]
+    )
+
+
+    const yfsp = new SiteClass(
+        'yfsp.tv',
+        [
+            'auth-label s',
+            'bl ng-star-inserted',
+            'ss-ctn',
+            'container-p',
+        ],
+        [
+            'commentBox',
+        ]
+    )
+
+
+    const movies = new SiteClass(
+        'movies',
+        [
+            'is-image',
+            'pc-content pc-ads',
+            'pc-home-swiper',
+            'pc-mask swiper-mask-circle',
+            'pc-home-swiper',
+            'inner position-relative w-100 h-100',
+            'inner position-relative w-100 h-100 ng-star-inserted',
+            'tab-live',
+            'vg-b',
+            'ads-mask-box',
+        ],
+        []
+    )
+
+
+    const sites = [
+        movies,
+        yfsp,
+        rophim,
+        olevod,
+    ];
+
+
+    // Periodically call hide() on each SiteClass instance
+    setInterval(() => {
+        for (const site of sites) {
+            site.hide();
+        }
+    }, 250);
+
+})();
+
+
+
