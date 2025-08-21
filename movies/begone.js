74f36ee72a244376ca4fd10f1d010b4c422a291a
Eric <58240560+naisanzaa@users.noreply.github.com>
2025-08-22 05:50:20 +0700

    Update begone.js

--- movies/begone.js
+++ movies/begone.js
@@ -65,7 +65,7 @@ class SiteClass {
 
     sites.push(new SiteClass(
         'rophim.me',
-        ['footer-elements','fade v-modal d-modal sspp-modal modal show','my-area','sspp-area is-post','app-download','denied-icon','item-v item-rate','item-v item-comment','v-line','v-rating','sspp-area is-3x2'],
+        ['footer-elements','fade v-modal d-modal sspp-modal modal show','my-area','sspp-area is-post','app-download','denied-icon','item-v item-rate','item-v item-comment','v-line','v-rating','sspp-area is-3x2','fade modal-backdrop show'],
         ['main_user','comment-area']
     ))
 
