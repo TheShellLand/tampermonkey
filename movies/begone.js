d3037c4e52ca91a7498f087385e877708f324b47
Eric <58240560+naisanzaa@users.noreply.github.com>
2025-09-09 09:33:02 -0400

    Update begone.js

--- movies/begone.js
+++ movies/begone.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         movies3
 // @namespace    http://tampermonkey.net/
-// @version      2025-08-11
+// @version      2025-09-09
 // @description  try to take over the world!
 // @author       You
 // @match        https://*/*
